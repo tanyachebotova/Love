20cfb3f2a78266c46f1082a41a08410d26d01c43
TatyanaChebotova <145840165+tanyachebotova@users.noreply.github.com>
2025-12-18 00:36:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-// Навигация между страницами
-const nextButtons = document.querySelectorAll('.next');
-const pages = document.querySelectorAll('.page');
-
-nextButtons.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        pages[index].classList.remove('active');
-        pages[index + 1].classList.add('active');
-    });
-});
-
-// Показать/скрыть текст
-const toggleButtons = document.querySelectorAll('.toggle');
-
-toggleButtons.forEach((btn) => {
+document.querySelectorAll('.toggle').forEach(btn => {
     btn.addEventListener('click', () => {
         const nextText = btn.nextElementSibling;
-        if (nextText.style.display === 'block') {
-            nextText.style.display = 'none';
-        } else {
-            nextText.style.display = 'block';
-        }
+        nextText.style.display = nextText.style.display === 'block' ? 'none' : 'block';
     });
 });
