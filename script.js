c235b8a2ac81e90778fcf385ae97dcaf5df12ba3
TatyanaChebotova <145840165+tanyachebotova@users.noreply.github.com>
2025-12-18 00:43:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// Кнопки "Показать больше"
 document.querySelectorAll('.toggle').forEach(btn => {
     btn.addEventListener('click', () => {
         const nextText = btn.nextElementSibling;
-        if(nextText.style.display === 'block') {
-            nextText.style.display = 'none';
-        } else {
-            nextText.style.display = 'block';
-        }
+        nextText.style.display = nextText.style.display === 'block' ? 'none' : 'block';
     });
 });
